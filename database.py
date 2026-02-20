d12b8300cf9811177ca431ba4221c5637eb8c8c2
sirulab <chensiru.sjtu@gmail.com>
2026-02-20 18:22:34 +0800

    update database

--- database.py
+++ database.py
@@ -4,9 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///database.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+else:
+    # Render.com PostgreSQL
+    engine = create_engine(DATABASE_URL)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
